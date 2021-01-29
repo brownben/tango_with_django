6c76487ead0006de835bfd14113712b36d0ef7b2
Ben Brown <2455263B@student.gla.ac.uk>
2021-01-29 13:42:40 +0000

    chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -12,14 +12,17 @@ def populate():
         {
             "title": "Official Python Tutorial",
             "url": "http://docs.python.org/3/tutorial/",
+            "views": 2,
         },
         {
             "title": "How to Think like a Computer Scientist",
             "url": "http://www.greenteapress.com/thinkpython/",
+            "views": 1,
         },
         {
             "title": "Learn Python in 10 Minutes",
             "url": "http://www.korokithakis.net/tutorials/python/",
+            "views": 3,
         },
     ]
 
@@ -27,14 +30,19 @@ def populate():
         {
             "title": "Official Django Tutorial",
             "url": "https://docs.djangoproject.com/en/2.1/intro/tutorial01/",
+            "views": 7,
         },
-        {"title": "Django Rocks", "url": "http://www.djangorocks.com/"},
-        {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/"},
+        {"title": "Django Rocks", "url": "http://www.djangorocks.com/", "views": 7},
+        {
+            "title": "How to Tango with Django",
+            "url": "http://www.tangowithdjango.com/",
+            "views": 7,
+        },
     ]
 
     other_pages = [
-        {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/"},
-        {"title": "Flask", "url": "http://flask.pocoo.org"},
+        {"title": "Bottle", "url": "http://bottlepy.org/docs/dev/", "views": 8},
+        {"title": "Flask", "url": "http://flask.pocoo.org", "views": 98},
     ]
 
     cats = {
@@ -46,7 +54,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(name=cat, views=cat_data["views"], likes=cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
